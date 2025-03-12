2a7f5871844f6b3d4d6fa1bcc7a5cc741c55525a
Cezary Klauza <cezaryklauza.dev@gmail.com>
2025-03-12 12:33:34 -0400

    Module 4

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # # path for registration 
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
